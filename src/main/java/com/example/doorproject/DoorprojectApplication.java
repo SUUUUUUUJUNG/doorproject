94b84231ccbb37c230b1926e89329b2249d7ff6a
SHIN SUJUNG <shinssjj79@gmail.com>
2025-02-09 18:54:51 +0900

    Initial commit

---
+++ src/main/java/com/example/doorproject/DoorprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.doorproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoorprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoorprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/doorproject/DoorprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doorproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoorprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
